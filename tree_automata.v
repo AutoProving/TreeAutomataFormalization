30f5e42ad71aa9cd5173e971417b5e8df81628cc
Ana <ana.agvb@gmail.com>
2020-07-19 20:19:47 +0100

    Discontinue structures based on nat
    The following have been renamed or redefined:
    - string is now the same as [r*]
    - prototree no longer exists
    - bprototree -> ptree

--- tree_automata.v
+++ tree_automata.v
@@ -6,38 +6,33 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-(*   This library is heavily based upon the mathcomp.ssreflect.seq library:    *)
+(*   This library is heavily based upon mathcomp.ssreflect libraries such as:  *)
 (*     https://math-comp.github.io/htmldoc/mathcomp.ssreflect.seq.html         *)
+(*     https://math-comp.github.io/htmldoc/mathcomp.ssreflect.fintype.html     *)
+(*     https://math-comp.github.io/htmldoc/mathcomp.ssreflect.tuple.html       *)
 (*                                                                             *)
 (*                                                                             *)
 (*   Here are short descriptions of currently implemented functionality.       *)
-(*   Let (r j k : nat), (p, q, s : string), (A : prototree), and               *)
-(* (U : bprototree r)                                                          *)
+(*   Let (r : nat), (j, k : [r]) (p, q, s : [r*]), and (U : ptree)             *)
 (*                                                                             *)
 (*                                 DATATYPES                                   *)
 (*              nat == the natural numbers                                     *)
 (*              [r] == the natural numbers smaller than r (aka the ordinal r)  *)
-(*           string == the type of finite strings (sequences) over the natural *)
-(*                     numbers                                                 *)
 (*             [r*] == the type of finite strings over [r]                     *)
-(*        prototree == the type of lists of finite strings over nat            *)
-(*     bprototree r == the type of lists of [r*]                               *)
+(*          ptree r == the type of lists of [r*]                               *)
 (*             [::] == the empty list (or string depending on its type)        *)
-(*           j :: p == a string corresponding to prepending the number j to    *)
-(*                     the string p                                            *)
+(*           j :: p == a string corresponding to prepending j to p             *)
 (* [:: j1; ...; jn] == the string with the elements j1 to jn                   *)
 (*                                                                             *)
 (*   The following coercions are available:                                    *)
 (*   - From [r] to nat                                                         *)
-(*   - From [r*] to string                                                     *)
-(*   - From bprototree r to prototree                                          *)
 (*                                                                             *)
 (*                              SUFFIX-CLOSED                                  *)
 (*         parent p == the string p without the first element, or [::] if p is *)
 (*                     empty                                                   *)
-(*  suffix_closed A == every sufix of an element of A is also an element of A  *)
-(*  well_numbered A == if (j :: p) is in A, then for every k <= j, the string  *)
-(*                     (k :: p) is also in A                                   *)
+(*  suffix_closed U == every sufix of an element of U is also an element of U  *)
+(*  well_numbered U == if (j :: p) is in U, then for every k <= j, the string  *)
+(*                     (k :: p) is also in U                                   *)
 (*                                                                             *)
 (*                                TREE-LIKE                                    *)
 (*      tree_like U == suffix-closed, well-numbered and without duplicates     *)
@@ -46,50 +41,40 @@ Unset Printing Implicit Defensive.
 (*  is_ancestor p q == p is a suffix of q                                      *)
 (*      ancestors p == every suffix of p                                       *)
 (* is_strict_ancestor p q == p is a suffix of q, but p is not q                *)
-(*     children A p == a list of all the children of p in A                    *)
-(*  descendants A p == a list of all the children of p, and the children of the*)
-(*                     children, and so on, as long as they are in A           *)
-(*      is_leaf A p == there are no descendants of p in A                      *)
-(*         leaves A == a list of all the leaves of A                           *)
-(*      connected A == there is only one string in A without its parent in A   *)
+(*     children U p == a list of all the children of p in U                    *)
+(*  descendants U p == a list of all the children of p, and the children of the*)
+(*                     children, and so on, as long as they are in U           *)
+(*      is_leaf U p == there are no descendants of p in U                      *)
+(*         leaves U == a list of all the leaves of U                           *)
+(*      connected U == there is only one string in U without its parent in U   *)
 
 
 
 
 (*   Unlike in the paper specification, here [r] is the finite set of numbers  *)
 (* between 0 and r-1, or in other words, the natural numbers i such that i < r.*)
-Notation "[ r ]" := 'I_r (at level 0).
+Notation "[ r ]" := 'I_r (at level 0, format "[ r ]").
 
 (*   There is an implicit coercion nat_of_ord : [r] -> nat that allows         *)
 (* functions on nat to seamleslly recieve inputs of type [r].                  *)
 
-(*   For now we define everything over the natural numbers instad of [r]       *)
-(* because it is better to compute with "raw" data types, i.e. datatypes       *)
-(* without proofs. As soon as this restriction is necessary we can add it and  *)
-(* make use of the coercion from [r] to nat to use the following definitions.  *)
 
-
-(*   We use lists of natural numbers to represent strings.                     *)
-Definition string := seq nat.
-
-(* ... and lists of elements of [r] to represent bounded strings, or [r*]      *)
-(*   We use the notation [r*] instead of [r]* because there could be parsing   *)
+(*   We use lists of elements of [r] to represent bounded strings, or [r*].    *)
+(* We use the notation [r*] instead of [r]* because there could be parsing     *)
 (* mistakes by parsing [r]* as the already existing notation [r] followed by *.*)
-Definition bstring (r : nat) := seq [r].
-Notation "[ r *]" := (bstring r) (at level 0).
+Definition string (r : nat) := seq [r].
+Notation "[ r *]" := (string r) (at level 0, format "[ r *]").
 
-Coercion string_of_bstring (r : nat) : [r*] -> string := map val.
+(* Pre-trees, i.e., structures that could be ill-formed trees *)
+Definition ptree (r : nat) := seq [r*].
 
-Definition prototree := seq string.
+Section Strings.
 
-Definition bprototree (r : nat) := seq [r*].
+Variable r : nat.
 
-Coercion prototree_of_bprototree (r : nat) : bprototree r -> prototree :=
-  map (@string_of_bstring r).
+Definition parent (p : [r*]) : [r*] := drop 1 p.
 
-Definition parent (p : string) : string := drop 1 p.
-
-Lemma parent_cons (p : string) (j : nat) : parent (j :: p) = p.
+Lemma parent_cons (p : [r*]) (j : [r]) : parent (j :: p) = p.
 Proof. by rewrite /parent /= drop0. Qed.
 Hint Resolve parent_cons : core.
 
@@ -101,12 +86,12 @@ Hint Resolve parent_nil : core.
 (* (ie, takes constant time) to drop the first element of a string but not the *)
 (* last. Thus throughout this development our strings are the reversed version *)
 (* of what appears in the paper specification.                                 *)
-Definition suffix_closed (U : prototree) : bool :=
+Definition suffix_closed (U : ptree r) : bool :=
   all (fun s => parent s \in U) U.
 
-Lemma suffix_closedP (U : prototree) :
+Lemma suffix_closedP (U : ptree r) :
   reflect
-    (forall (p : string) (j : nat), j :: p \in U -> p \in U)
+    (forall (p : [r*]) (j : [r]), j :: p \in U -> p \in U)
     (suffix_closed U).
 Proof.
   rewrite /suffix_closed.
@@ -118,7 +103,7 @@ Proof.
   by rewrite parent_cons; apply: H.
 Qed.
 
-Lemma suffix_closed_correct (U : prototree) (p : string) (n : nat) :
+Lemma suffix_closed_correct (U : ptree r) (p : [r*]) (n : nat) :
   suffix_closed U -> p \in U -> drop n p \in U.
 Proof.
   move=> /allP scU; move: p; elim: n => [p | n IH].
@@ -128,7 +113,7 @@ Proof.
   by rewrite -(parent_cons p j); apply: scU.
 Qed.
 
-Lemma suffix_closed_nil (U : prototree) :
+Lemma suffix_closed_nil (U : ptree r) :
   suffix_closed U -> U != [::] -> [::] \in U.
 Proof.
   case: U => [// | s U closedsU _].
@@ -136,52 +121,60 @@ Proof.
   by apply: suffix_closed_correct => //; rewrite in_cons eqxx.
 Qed.
 
-Definition well_numbered_single (U : prototree) (s : string) : bool :=
+Definition subon (j : [r]) (n : nat) : [r].
+Proof.
+  refine (@Ordinal r (j - n) _).
+  by move: j => [j jltr]; rewrite (leq_ltn_trans _ jltr) ?leq_subr.
+Defined.
+Notation "j -on k" := (subon j k) (at level 50, format "j  -on  k").
+
+
+Definition well_numbered_single (U : ptree r) (s : [r*]) : bool :=
   match s with
   | [::] => true
-  | j :: p => j.-1 :: p \in U
+  | j :: p => (j -on 1) :: p \in U
   end.
 
-Definition well_numbered (U : prototree) : bool :=
+Definition well_numbered (U : ptree r) : bool :=
   all (well_numbered_single U) U.
 
-Lemma well_numberedP (U : prototree) :
+Lemma well_numberedP (U : ptree r) :
   reflect
-    (forall (p : string) (j : nat),
-        j :: p \in U -> forall (k: nat), k <= j -> k :: p \in U)
+    (forall (p : [r*]) (j : [r]),
+        j :: p \in U -> forall (k : [r]), k <= j -> k :: p \in U)
     (well_numbered U).
 Proof.
   apply: (iffP idP).
-    move=> /allP; rewrite /well_numbered_single => wnU p j jpinU k kleqj.
-    set i := (j - k).
-    have: (k = j - i) by rewrite /i subKn.
-    move=> ->.
+    move=> /allP /=; rewrite /well_numbered_single => wnU p j jpinU k kleqj.
+    set i := j - k.
+    have -> : k = j -on i by apply: val_inj; rewrite /i /= subKn.
     elim: i => [| n IH].
-      by rewrite subn0.
-    rewrite subnS.
+      suff -> : j -on 0 = j => //.
+      by apply: val_inj; rewrite /= subn0.
+    have aa := subnS.
+    have -> : j -on n.+1 = j -on n -on 1.
+      by apply: val_inj; rewrite /= subnS subn1.
     by apply: wnU _ IH.
-  move=> H.
-  apply /allP => p.
-  case: p =>// a l alinU.
-  by apply: (H _ a) => //; apply: leq_pred.
- Qed.
+  move=> H; apply /allP; case => // a l alinU.
+  by apply: (H _ a) => //; apply: leq_subr.
+Qed.
 
 
-Definition tree_like (r : nat) (U : bprototree r) : bool :=
+Definition tree_like (U : ptree r) : bool :=
   [&& suffix_closed U, well_numbered U & uniq U].
 
-Record tree (r : nat) := Tree {
-  bprototree_of_tree :> bprototree r;
-  _ : tree_like bprototree_of_tree
+Record tree := Tree {
+  ptree_of_tree :> ptree r;
+  _ : tree_like ptree_of_tree
 }.
 
 (* p is a parent of q *)
-Definition is_parent (p q : string) : bool := parent p == q.
+Definition is_parent (p q : [r*]) : bool := parent p == q.
 
 (* p is a child of q *)
-Definition is_child (p q : string) : bool := is_parent q p.
+Definition is_child (p q : [r*]) : bool := is_parent q p.
 
-Definition is_ancestor (p q : string) : bool :=
+Definition is_ancestor (p q : [r*]) : bool :=
   p == drop (size q - size p) q.
 
 Lemma is_ancestorpp p : is_ancestor p p.
@@ -189,10 +182,10 @@ Proof.
   by rewrite /is_ancestor subnn drop0.
 Qed.
 
-Definition ancestors (s : string) : seq string :=
+Definition ancestors (s : [r*]) : seq [r*] :=
   [seq drop i s | i <- iota 0 (size s).+1].
 
-Lemma suffix_closed_ancestors (U : prototree) (p : string) :
+Lemma suffix_closed_ancestors (U : ptree r) (p : [r*]) :
   suffix_closed U -> p \in U -> all (mem U) (ancestors p).
 Proof.
   move=> scU pinU; rewrite /ancestors.
@@ -200,7 +193,7 @@ Proof.
   by apply: suffix_closed_correct.
 Qed.
 
-Lemma is_ancestor_ancestors (p q : string) :
+Lemma is_ancestor_ancestors (p q : [r*]) :
   is_ancestor p q -> p \in ancestors q.
 Proof.
   rewrite /is_ancestor /ancestors => /eqP ancp.
@@ -209,66 +202,67 @@ Proof.
   by rewrite mem_iota add0n ltnS leq_subr.
 Qed.
 
-Definition is_strict_ancestor (p q : string) : bool :=
+Definition is_strict_ancestor (p q : [r*]) : bool :=
   let d := (size q - size p) in
   (d != 0) && (p == drop d q).
 
-Lemma is_strict_ancestorW (p q : string) :
+Lemma is_strict_ancestorW (p q : [r*]) :
   is_strict_ancestor p q -> is_ancestor p q.
 Proof. by move=> /andP []. Qed.
 
-Lemma self_ancestor (s : string) : s \in ancestors s.
+Lemma self_ancestor (s : [r*]) : s \in ancestors s.
 Proof.
   rewrite /ancestors.
   set f := drop^~ s; have -> : s = f 0 by rewrite /f drop0.
   by apply: map_f; rewrite mem_iota.
 Qed.
 
-Definition children  (U : prototree) (p : string) : seq string :=
+Definition children  (U : ptree r) (p : [r*]) : seq [r*] :=
   [seq s <- U | is_parent p s].
 
-Definition descendants (U : prototree) (p : string) : seq string :=
+Definition descendants (U : ptree r) (p : [r*]) : seq [r*] :=
   [seq s <- U | is_ancestor p s].
 
-Definition is_leaf (U : prototree) (s : string) :=
+Definition is_leaf (U : ptree r) (s : [r*]) :=
   all (fun p => ~~ (is_strict_ancestor s p)) U.
 
-Definition leaves (U : prototree) : seq string :=
+Definition leaves (U : ptree r) : seq [r*] :=
   [seq s <- U | is_leaf U s].
 
 (* TODO maybe the empty S should also be connected *)
-Definition connected (S : prototree) : bool :=
+Definition connected (S : ptree r) : bool :=
   count (fun p => (p == [::]) || (parent p \notin S)) S == 1.
 
-Lemma connected_correct (S : prototree) (p : string) :
+Lemma connected_correct (S : ptree r) (p : [r*]) :
   (p == [::]) || (parent p \notin S) =
   (p == [::]) || (p != [::]) && (parent p \notin S).
 Proof. by case: p. Qed.
 
-Require Import Logic.FunctionalExtensionality.
+End Strings.
+
 Section Terms.
 
 Variable r : nat.
 Variable X : finType.
 Variable d : X.
 
-Record prototerm := Prototerm {
-  pos : bprototree r;
-  assignment_of_prototerm :> [r*] -> X;
+(* Pre-terms, i.e., terms whose pos is not necessarily valid. *)
+Record pterm := Pterm {
+  pos : ptree r;
+  assignment_of_pterm :> [r*] -> X;
 }.
 
-Definition prototerm_code (t : prototerm) : seq ([r*] * X) :=
+Definition pterm_code (t : pterm) : seq ([r*] * X) :=
   zip (pos t) (map t (pos t)).
 
-Definition prototerm_decode (AX : seq ([r*] * X)) : prototerm :=
-  Prototerm (unzip1 AX) (fun s => (nth d (unzip2 AX) (index s (unzip1 AX)))).
+Definition pterm_decode (AX : seq ([r*] * X)) : pterm :=
+  Pterm (unzip1 AX) (fun s => (nth d (unzip2 AX) (index s (unzip1 AX)))).
 
-
-Lemma prototerm_codeK (t : prototerm) (s : [r*]) :
-  s \in pos t -> prototerm_decode (prototerm_code t) s = t s.
+Lemma pterm_codeK (t : pterm) (s : [r*]) :
+  s \in pos t -> pterm_decode (pterm_code t) s = t s.
 Proof.
   move: t => [post t].
-  rewrite /prototerm_code /prototerm_decode /=.
+  rewrite /pterm_code /pterm_decode /=.
   rewrite unzip2_zip ?size_map // unzip1_zip ?size_map //.
   elim: post => [// | a A IH].
   rewrite in_cons /=.
@@ -276,41 +270,75 @@ Proof.
   by rewrite eq_sym => ->.
 Qed.
 
-Definition eqt : rel prototerm := fun t1 t2 =>
-  prototerm_code t1 == prototerm_code t2.
-Notation "t1 =t t2" := (eqt t1 t2) (at level 70).
+Definition eqt : rel pterm := fun t1 t2 =>
+  pterm_code t1 == pterm_code t2.
+Notation "t1 =t t2" := (eqt t1 t2) (at level 70, format "t1  =t  t2").
 
-Definition build_prototerm (a : X) (ts : r.-tuple prototerm) : prototerm :=
-  let post := [::] ::
-    flatten [seq [seq rcons p j | p <- pos (tnth ts j)] | j <- ord_enum r]
-  in
+Definition build_ptree (trees : r.-tuple (ptree r)) : ptree r :=
+  [::] :: [seq rcons p j | j <- ord_enum r, p <- (tnth trees j)].
+
+Lemma rcons_nil (T : eqType) (p : seq T) (j : T) : (rcons p j == [::]) = false.
+Proof. by case: p. Qed.
+Hint Resolve rcons_nil : core.
+
+Lemma build_ptreeE (trees : r.-tuple (ptree r)) (p : [r*]) (j : [r]) :
+  rcons p j \in build_ptree trees = (p \in (tnth trees j)).
+Proof.
+  rewrite /build_ptree in_cons rcons_nil /=.
+  have [pintreesj |] := boolP (p \in _).
+    by apply /allpairsPdep; exists j; exists p; rewrite mem_ord_enum.
+  apply: contraNF.
+  by move=> /allpairsPdep /= [k [s [_ sintreesk /rcons_inj [-> ->]]]].
+Qed.
+
+Lemma tree_like_build (trees : r.-tuple (ptree r)) :
+    all (fun tr => tr != [::]) trees ->
+    all (@tree_like r) trees ->
+  tree_like (build_ptree trees).
+Proof.
+  move=> /allP /= nonempty /allP /=.
+  rewrite /tree_like => validtrees; apply /and3P; split.
+  - rewrite /build_ptree.
+    apply /suffix_closedP => s i.
+    have /= -> := in_cons [::] _ (_ :: _).
+    case: s => [// | j s].
+    rewrite in_cons /= (lastI j).
+    have /= mapf := map_f (rcons (belast j s)).
+    admit.
+  - apply /well_numberedP; case => [j | j p i].
+      move=> _ k kltj; rewrite /build_ptree.
+      rewrite lastI build_ptreeE /=.
+      move: (validtrees (tnth trees k) (mem_tnth _ _)) => /and3P [sc _ _].
+      apply: suffix_closed_nil => //.
+      by apply: (nonempty _ (mem_tnth _ _)).
+    rewrite lastI build_ptreeE /= => H k klti.
+    rewrite lastI build_ptreeE /=.
+    move: (validtrees (tnth _ (last j p)) (mem_tnth _ _)).
+    move=> /and3P [_ /well_numberedP wn _].
+    by apply: wn klti.
+  - rewrite /build_ptree cons_uniq; apply /andP; split.
+      by apply /allpairsPdep => [[j [p [_ _ /eqP]]]]; rewrite eq_sym rcons_nil.
+    apply: allpairs_uniq_dep.
+    + exact: ord_enum_uniq.
+    + by move=> k _; move: (validtrees (tnth _ k) (mem_tnth _ _)) => /and3P [].
+    + by move=> /= [j1 p1] [j2 p2] _ _ /rcons_inj [p1e1p2 j1eqj2]; f_equal.
+Admitted.
+
+Definition build_pterm (a : X) (ts : r.-tuple pterm) : pterm :=
+  let post := build_ptree [tuple of map pos ts] in
   let t (s : [r*]) :=
     match rev s with
     | [::] => a
     | j :: p => (tnth ts j) (rev p)
     end
   in
-  Prototerm post t.
+  Pterm post t.
 
 (* FIXME This is silly because pos could have type tree *)
 Record term := Term {
-  term_of_prototerm :> prototerm;
-  _ : tree_like (pos term_of_prototerm);
+  term_of_pterm :> pterm;
+  _ : tree_like (pos term_of_pterm);
 }.
-
-Lemma tree_like_build (a : X) (ts : r.-tuple prototerm) :
-    all (@tree_like r) (map pos ts) ->
-  tree_like (pos (build_prototerm a ts)).
-Proof.
-  move=> /allP /= H.
-  rewrite /tree_like; apply /and3P; split.
-  - apply /suffix_closedP => p j.
-    rewrite 2!in_cons /=.
-    case: p => //=.
-    admit.
-  - admit.
-  - admit.
-Admitted.
 
 (*
 Variable state : finType.
@@ -381,4 +409,3 @@ Definition L (A : bu_tree_automata) (q : states) (i : nat) : seq (terms X) :=
 Inductive states :=
   | base : nat -> states
   | build : states * states -> states.
-Check build (build (base 0, base 1), base 2).
